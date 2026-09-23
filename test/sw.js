7fe6d32563179bfe1c5bb3f327c120980520ca48
ShiftpayHQ <jamesross006@gmail.com>
2026-09-23 14:26:52 +0100

    Add files via upload

--- test/sw.js
+++ test/sw.js
@@ -1,3 +1,3 @@
-// Shift Pay HQ v9_10_3au DEV cache cleanup worker.
+// Shift Pay HQ v9_10_3av DEV cache cleanup worker.
 self.addEventListener("install", event => { self.skipWaiting(); });
-self.addEventListener("activate", event => { event.waitUntil((async()=>{ const keys=await caches.keys(); await Promise.all(keys.filter(k=>k.startsWith("shift-pay-hq-")).map(k=>caches.delete(k))); await self.registration.unregister(); const cs=await self.clients.matchAll({type:"window",includeUncontrolled:true}); for(const c of cs)c.postMessage({type:"SPHQ_DEV_SW_REMOVED",version:"v9_10_3au"}); })()); });
+self.addEventListener("activate", event => { event.waitUntil((async()=>{ const keys=await caches.keys(); await Promise.all(keys.filter(k=>k.startsWith("shift-pay-hq-")).map(k=>caches.delete(k))); await self.registration.unregister(); const cs=await self.clients.matchAll({type:"window",includeUncontrolled:true}); for(const c of cs)c.postMessage({type:"SPHQ_DEV_SW_REMOVED",version:"v9_10_3av"}); })()); });
